2b97339b81fa19b4e2bc9dfeb9adfaa91dd3335b
zasmi5421 <zahir@Pussydestroyer.localdomain>
2026-03-28 07:35:25 +0100

    small modification for simpler understanding

--- codec_repetition_encode.c
+++ codec_repetition_encode.c
@@ -4,6 +4,6 @@
 
 void codec_repetition_encode(uint8_t *U_k,  uint8_t *C_N, size_t K, size_t n_reps){
     for(size_t i=0; i < K*n_reps; i++){
-        C_N[i] = U_k[i - (i/K)*K];
+        C_N[i] = U_k[i % K];
     }
 }
